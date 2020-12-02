da03f8ccc317d0eec72ae59e3e0334a1246eb151
varnerlab <jdv27@cornell.edu>
2020-12-02 18:18:57 -0500

    Update Data.jl
    working on model

--- src/Data.jl
+++ src/Data.jl
@@ -1,10 +1,19 @@
-function build_default_data_dictionary()
+function build_data_dictionary()
 
     # Initialize -
     data_dictionary = Dict{String,Any}()
 
+    # setup system dimension -
+
+    # setup initial conditions -
+    initial_condition_array = [
+
+    ]
+
+
     # --- DO NOT EDIT BELOW THIS LINE ----------------------------------------------- #
-    data_dictionary["initial_condition_array"] = nothing
+    data_dictionary["initial_condition_array"] = initial_condition_array
+    
     return data_dictionary
     # --- DO NOT EDIT ABOVE THIS LINE ----------------------------------------------- #
 end
